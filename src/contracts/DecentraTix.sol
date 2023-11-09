97e8a0d2feb9304f9128a987e18342cc12ed9dd8
Ethan <ethanhuang03@gmail.com>
2023-11-09 15:38:19 +0800

    Rename Contract Change artist variable to string Make subscriber mapping address => uint256

--- src/contracts/DecentraTix.sol
+++ src/contracts/DecentraTix.sol
@@ -3,10 +3,9 @@ pragma solidity ^0.8.22;
 
 import "./Ownable.sol";
 
-contract DecentralizedTicketingPlatform is Ownable {
+contract DecentraTix is Ownable {
     
     struct Ticket {
-        address artist;
         uint256 eventDate;
         uint256 eventTime;
         uint256 row;
@@ -21,7 +20,7 @@ contract DecentralizedTicketingPlatform is Ownable {
     mapping(uint256 => Ticket) public tickets;
     
     // Mapping of artist to subscribed addresses
-    mapping(address => mapping(address => uint256)) public artistSubscriptions;
+    mapping(address => uint256) public activeSubscriptions;
     
     // Mapping of address to list of owned tickets
     mapping(uint256 => address) public ticketToOwner;
@@ -31,18 +30,21 @@ contract DecentralizedTicketingPlatform is Ownable {
     uint256 public resaleTaxPercentage = 5;
     bool public isResaleAllowed = true;
     bool public isTransferAllowed = true;
+    bool public isPublicSale;
+    string public artist;
     
     // Events
     event TicketCreated(uint256 ticketId);
     event TicketBought(address buyer, uint256 ticketId);
-    event SubscriptionBought(address subscriber, address artist);
+    event SubscriptionBought(address subscriber);
     
-    modifier onlySubscribed(address artist) {
-        require(block.timestamp - artistSubscriptions[artist][msg.sender] < 30 days, "You are not subscribed to this artist");
+    modifier onlySubscriber() {
+        require(block.timestamp - activeSubscriptions[msg.sender] < 30 days, "You are not currently subscribed to this artist");
         _;
     }
 
-    constructor() Ownable(tx.origin) {
+    constructor(string memory _artist) Ownable(tx.origin) {
+        artist = _artist;
     }
 
     receive() external payable {
@@ -54,7 +56,6 @@ contract DecentralizedTicketingPlatform is Ownable {
     
     // Contract Owner functions
     function createTicket(
-        address _artist,
         uint256 _eventDate,
         uint256 _eventTime,
         uint256 _row,
@@ -62,7 +63,7 @@ contract DecentralizedTicketingPlatform is Ownable {
         uint256 _price,
         string memory section
     ) public onlyOwner {
-        tickets[totalTickets] = Ticket(_artist, _eventDate, _eventTime, _row, _seat, _price, totalTickets, section, false);
+        tickets[totalTickets] = Ticket(_eventDate, _eventTime, _row, _seat, _price, totalTickets, section, false);
         emit TicketCreated(totalTickets);
         totalTickets++;
     }
@@ -82,18 +83,20 @@ contract DecentralizedTicketingPlatform is Ownable {
     function toggleTransferAllowed() public onlyOwner {
         isTransferAllowed = !isTransferAllowed;
     }
+
+    function togglePublicSale() public onlyOwner {
+        isPublicSale = !isPublicSale;
+    }
     
     // User functions
-    function buyTicket(uint256 ticketId) public payable {
+    function buyTicket(uint256 ticketId) private {
         uint256 ticketPrice = tickets[ticketId].price;
         require(!(tickets[ticketId].isPurchased), "Ticket already purchased");
         require(msg.value >= ticketPrice, "Insufficient payment");
-        
-        if(block.timestamp + 2 days < tickets[ticketId].eventDate) {
-            require(block.timestamp - artistSubscriptions[tickets[ticketId].artist][msg.sender] < 30 days, "You are not subscribed to this artist");
-        }
+
         (bool success,) = address(this).call{value: msg.value}("");
         require(success, "Ticket cannot be purchased");
+
         tickets[ticketId].isPurchased = true;
         ticketToOwner[ticketId] = msg.sender;
         emit TicketBought(msg.sender, ticketId);
@@ -101,12 +104,21 @@ contract DecentralizedTicketingPlatform is Ownable {
             payable(msg.sender).transfer(msg.value - ticketPrice);
         }
     }
+
+    function buyTicketSubscriber(uint256 ticketId) public payable onlySubscriber() {
+        buyTicket(ticketId);
+    }
+
+    function buyTicketPublic(uint256 ticketId) public payable {
+        require(isPublicSale, "Public sale is not open yet");
+        buyTicket(ticketId);
+    }
     
-    function subscribeToArtist(address artist) public payable {
+    function subscribeToArtist() public payable {
         require(msg.value > 0, "Subscription fee required");
         
-        artistSubscriptions[artist][msg.sender] = block.timestamp;
-        emit SubscriptionBought(msg.sender, artist);
+        activeSubscriptions[msg.sender] = block.timestamp;
+        emit SubscriptionBought(msg.sender);
     }
     
     function resaleTicket(uint256 ticketId, uint256 newPrice) public {
