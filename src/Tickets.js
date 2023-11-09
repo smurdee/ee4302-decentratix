c575dec840a60f7e8dd90e179b1e383bfd7c873d
nonrnp <pichanon2545@gmail.com>
2023-11-09 21:20:16 +0800

    add section to show owned tickets and tickets on the marketplace

--- src/Tickets.js
+++ src/Tickets.js
@@ -3,36 +3,56 @@ import React, { Component } from 'react'
 class Market extends Component {
 	
   handleButtonClick = (ticket) => {
-    // Your logic when the button is clicked
     console.log(`Button clicked for ticket with content: ${ticket.content}`);
     // Add your custom logic here
   };	
 
   render() {
     return (
-      <div id="content" style={{ textAlign: 'right', paddingLeft: '160px' }}> 
-        <form onSubmit={(event) => {
-          event.preventDefault()
-          this.props.createTicket(this.ticket.value)
-        }}>
-          <input id="newTicket" ref={(input) => this.ticket = input} type="text" className="form-control" placeholder="Add ticket..." required />
-          <input type="submit" hidden={true} />
-        </form>
-        <ul id="ticketList" className="list-unstyled">
-          { this.props.tickets.map((ticket, key) => {
-            return(
-              <div className="ticketTemplate" className="checkbox" key={key}>
-                <label>
-                  <span className="content">{ticket.content}</span>
-				  <button onClick={() => this.handleButtonClick(ticket)}>Buy This Ticket</button>
-                </label>
-              </div>
-            )
-          })}
-        </ul>
-        <ul id="completedTicketList" className="list-unstyled">
-        </ul>
-      </div>
+	  <div id="content" class="cntainer"> 
+		<div class="row">
+		  <div id="mytickets" class="col-md-6"> 
+		    <h1>My Tickets</h1>
+			<ul id="ticketList" className="list-unstyled">
+			  { this.props.tickets.map((ticket, key) => {
+				return(
+				  <div className="ticketTemplate" className="checkbox" key={key}>
+					<label>
+					  <span className="marketcontent">{ticket.content}</span>
+					</label>
+				  </div>
+				)
+			  })}
+			</ul>
+		  </div>
+
+		  <div id="marketcontent" class="col-md-6"> 
+			<h1>Marketplace</h1>			
+			<ul id="ticketList" className="list-unstyled">
+			  { this.props.tickets.map((ticket, key) => {
+				return(
+				  <div className="ticketTemplate" className="checkbox" key={key}>
+					<label>
+					  <span className="marketcontent">{ticket.content}</span>
+					  <button onClick={() => this.handleButtonClick(ticket)}>Buy This Ticket</button>
+					</label>
+				  </div>
+				)
+			  })}
+			</ul>
+			<ul id="completedTicketList" className="list-unstyled">
+			</ul>
+			<form onSubmit={(event) => {
+			  event.preventDefault()
+			  this.props.createTicket(this.ticket.value)
+			}}>
+			  <input id="newTicket" ref={(input) => this.ticket = input} type="text" className="form-control" style={{ width: '200px'}} placeholder="Add ticket..." required />
+			  <input type="submit" hidden={true} />
+			</form>
+		  </div>	
+		</div>
+	  </div>
+		
     );
   }
 }
